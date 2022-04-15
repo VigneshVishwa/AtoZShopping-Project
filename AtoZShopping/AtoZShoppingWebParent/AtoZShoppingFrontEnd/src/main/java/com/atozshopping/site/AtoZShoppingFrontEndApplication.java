61ff916719a1a0a2f654b10235c65764c41db572
VigneshVishwa <vigneshvishwa1234@gmail.com>
2022-04-15 15:20:51 +0530

    My First Commit

---
+++ AtoZShopping/AtoZShoppingWebParent/AtoZShoppingFrontEnd/src/main/java/com/atozshopping/site/AtoZShoppingFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.atozshopping.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtoZShoppingFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtoZShoppingFrontEndApplication.class, args);
+	}
+
+}
